b51a2bff6cff85301ce9c5b4ff5d4b2c0f5a97a3
chad <pickallpickle@gmail.com>
2021-11-04 17:37:35 +0900

    튜토리얼 진행 완료.

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/ex/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/ex/basicscodelab2/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,14 +29,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            BasicsCodelab2Theme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(color = MaterialTheme.colors.background) {
-//                    Greeting("Android")
-//                }
-//            }
             BasicsCodelab2Theme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -50,15 +50,25 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+        // note : remember는 메모리에 저장? 생명주기 확인 필요.
+        var isExpanded by remember { mutableStateOf(false) }
+        // note: remember와 state의 차이?
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2)
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.medium,
+            elevation = 1.dp,
+            color = surfaceColor,
+            modifier = Modifier.animateContentSize().padding(1.dp)) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
@@ -74,19 +84,15 @@ fun MessageCard(msg: Message) {
 @Composable
 fun PreviewMessageCard() {
     BasicsCodelab2Theme {
-        MessageCard(Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
+        Conversation(messages = SampleData.conversationSample)
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+           message -> MessageCard(msg = message)
+        }
+    }
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    BasicsCodelab2Theme {
-//        Greeting("Android")
-//    }
-//}
